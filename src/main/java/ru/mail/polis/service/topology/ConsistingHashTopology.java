bd3e88abfe0a93cb88abd41b854f9020dfc18cf0
arman-sh <rosko1245@gmail.com>
2019-10-27 20:36:26 +0300

    code climate

--- src/main/java/ru/mail/polis/service/topology/ConsistingHashTopology.java
+++ src/main/java/ru/mail/polis/service/topology/ConsistingHashTopology.java
@@ -131,7 +131,7 @@ public final class ConsistingHashTopology implements Topology<ServiceNode> {
         public long hash(@NotNull final ByteBuffer key) {
             MessageDigest messageDigest;
             try {
-                messageDigest = MessageDigest.getInstance("MD5");
+                messageDigest = MessageDigest.getInstance("MD5"); // MD5 hash
                 messageDigest.reset();
                 final ByteBuffer duplicate = key.duplicate();
                 final byte[] bytes = new byte[duplicate.remaining()];
